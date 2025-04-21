956440c6a6db04389f8dda17e615f8b180e2f77c
Alec Diramerian <adiramerian@gmail.com>
2025-04-21 10:29:19 -0700

    alec clicker update

---
+++ javascript/morevariables.js
@@ -0,0 +1,200 @@
+export const news = [
+	`News: Escaped Alec doesn't wear flannel, global population terrified`,
+	`News: PSAT app turns man into a smurf`,
+	`News: "𝘐 𝘩𝘢𝘥 𝘢 𝘤𝘳𝘶𝘴𝘩 𝘰𝘯 𝘵𝘩𝘦 𝘮𝘰𝘰𝘯 𝘧𝘰𝘳 𝘺𝘦𝘢𝘳𝘴, 𝘢𝘯𝘥 𝘵𝘩𝘦𝘯 𝘐 𝘧𝘪𝘯𝘢𝘭𝘭𝘺 𝘮𝘢𝘳𝘳𝘪𝘦𝘥 𝘩𝘦𝘳... 𝘢𝘯𝘥 𝘵𝘩𝘦𝘯 𝘴𝘰𝘮𝘦𝘰𝘯𝘦 𝘚𝘛𝘖𝘓𝘌 𝘐𝘛!", says Fester in an interview`,
+	`News: Alec Clicker receives a long awaited update`,
+	`News: Middle School Bridges Confessions taken down, students saddened`,
+	`News: Shoe gets thrown at fundraiser guy, students agree it was justified`,
+	`News: Something Rotten is a smashing success!`,
+	`News: Addams Family is a smashing success!`,
+	`News: Iceberg causes a school panic... by school, we mean 4 or 5 people`,
+	`News: Student goes to Boulder, Colorado, meets Dr. Nefario`,
+	`News: Student almost beats a younger student to death with a water bottle`,
+	`News: Student jams vending machine, older students outraged`,
+	`News: Student gets so tired, eats horse`,
+	`News: Student needs HEEEELP, professionals in the field of help say nothing can be done, as he needs HEEEELP, not help`,
+	`News: Student at autistic school comes out as straight... wait, WHAT?`,
+	`News: Student jams vending machine, older students outraged`,
+	`News: Student swallows a bottle of alcohol, feels like Godzilla`,
+	`News: Student is a shadow of his former self after waiting for Deltarune`,
+	`News: Student fails to fill thier role in musical, students pissed`,
+	`News: Student dates his bestie after breaking up with monkey, assures peers the partner is differant`,
+	`News: Student watches grass grow for 2 days straight. "𝘔𝘳. 𝘎𝘳𝘢𝘴𝘴 𝘴𝘵𝘰𝘭𝘦 𝘮𝘺 𝘴𝘩𝘢𝘷𝘪𝘯𝘨 𝘤𝘳𝘦𝘢𝘮 𝘢𝘯𝘥 𝘵𝘩𝘦 2 𝘶𝘯𝘨𝘶𝘴", says the student`,
+	`News: Student summons Persona 4 character, kidnaps him and forcefully allows another certain student to pass away`,
+	`News: Students fail to cut ribbon, other students giggle`,
+	`News: Teacher turns his big Alec into himself after requesting it for years, gets filled with joy`,
+	`News: Theodore codes for BridgesHOF.com, goes insane`,
+	`News: Short film actually gets made, experts baffled`,
+	`News: Man watches Cooking With Alec, gets hospitalized`,
+	`News: Mark Long reveals his alter ego to be Marcus Length, students starstruck`,
+	`News: I CALLED MARK LONG AT 3AM (GONE WRONG) (COPS CALLED) 😲😲😲`,
+	`News: Scott has been notified about your Alec buisness`,
+	`News: Researchers provide evidence that Chris is more powerful than God, Jod, Thor, Zeus, Allah, and Buddha, all combined`,
+	`Quote from an Asylum: "𝘙𝘰𝘰𝘮𝘴? 𝘐 𝘸𝘢𝘴 𝘳𝘰𝘰𝘮𝘴 𝘰𝘯𝘤𝘦. 𝘛𝘩𝘦𝘺 𝘤𝘳𝘢𝘻𝘪𝘦𝘥 𝘮𝘦 𝘪𝘯 𝘢 𝘭𝘰𝘤𝘬. 𝘈 𝘳𝘢𝘵𝘴 𝘭𝘰𝘤𝘬. 𝘈 𝘳𝘢𝘵𝘴 𝘭𝘰𝘤𝘬𝘦𝘥 𝘧𝘶𝘭𝘭 𝘰𝘧 𝘤𝘳𝘢𝘻𝘺, 𝘢𝘯𝘥 𝘤𝘳𝘢𝘻𝘺 𝘮𝘢𝘬𝘦𝘴 𝘮𝘦 𝘳𝘰𝘰𝘮𝘴", says man convicted for ratslaughter`,
+	`Quote from an Asylum: "𝘔𝘳. 𝘓𝘰𝘯𝘨... 𝘐 𝘥𝘰𝘯'𝘵 𝘧𝘦𝘦𝘭 𝘴𝘰 𝘨𝘰𝘰𝘥", says insect-eating teen`,
+	`Quote from an Asylum: "𝘏𝘌𝘌𝘌𝘌𝘓𝘗! 𝘏𝘌𝘓𝘗 𝘔𝘌!", says boy who doesn't even live in the asylum, but he's powerful enough to teleport in there`,
+	`Strange beings come from distant planets to buy your Alecs`,
+	`Pop culture loves your Alecs!`,
+	`Brainrot culture is raving about your Alecs!`,
+];
+
+export const tips = [
+	`Click the big picture of Alec to start gaining Alecs!`,
+	`Buy upgrades to automatically create Alecs!`,
+	`Hover your cursor over everything, It might have something to say!`,
+	`The game saves every time the Alec is clicked, every time an upgrade is bought, and every 10 seconds!`,
+	`Follow @bridges_hof on Instagram for Alec Clicker and Bridges Hall of Fame related news!`,
+	`Find a bug or have a suggestion? Create an issue or pull request on GitHub!`,
+	`Need to leave for a while? You can keep the tab open and do something on another window, your upgrades still work!`,
+	`Everything saves! Some values from before v2.0 them won't be listed on the stats tab due to them not being tracked.`,
+	`Want to play on the go? Alec Clicker (kinda) works on mobile! Unfortunately, your stats will differ.`,
+	`You can click the newsbar to advance it!`,
+	`More content and tips will come in the future!`,
+];
+
+export const achievements = [
+	// clicks
+	{name: "It begins...", description: `Click the big Alec once<br><i>"He awakens."</i>`, achieved: false},
+	{name: "Kid named finger", description: "Click the big Alec 100 times", achieved: false},
+	{name: "Tired yet?", description: "Click the big Alec 1,000 times", achieved: false},
+	{name: "Finger infection", description: `Click the big Alec 1,000,000 times<br><i>"Please don't..."</i>`, achieved: false},
+
+	// ac1 (autoclicker)
+	{name: "Click", description: "Buy an autoclicker", achieved: false},
+	{name: "Double click", description: "Buy 2 autoclickers", achieved: false},
+	{name: "Dozen click", description: "Buy 12 autoclickers", achieved: false},
+	{name: "Quarter click", description: "Buy 25 autoclickers", achieved: false},
+	{name: "Half click", description: "Buy 50 autoclickers", achieved: false},
+	{name: "Full click", description: "Buy 100 autoclickers", achieved: false},
+
+	// ac2 (factory)
+	{name: "Womber", description: "Buy a factory", achieved: false},
+	{name: "It's twins!", description: "Buy 2 factories", achieved: false},
+	{name: "Loan maker", description: "Buy 10 factories", achieved: false},
+	{name: "Assembler", description: "Buy 25 factories", achieved: false},
+	{name: "FORTY?", description: "Buy 40 factories", achieved: false},
+	{name: "Why him?", description: "Buy 100 factories", achieved: false},
+
+	// ac3 (sewing kit)
+	{name: "Granny", description: "Buy a sewing kit", achieved: false},
+	{name: "Grampy", description: "Buy 5 sewing kits", achieved: false},
+	{name: "They/them grandparent", description: "Buy 10 sewing kits", achieved: false},
+	{name: "Aunt", description: "Buy 25 sewing kits", achieved: false},
+	{name: "Unc", description: "Buy 50 sewing kits", achieved: false},
+	{name: "They/them sibling of parent", description: `Buy 100 sewing kits<br><i>"It's kinda interesting how there isn't a word for this, isn't it?"</i>`, achieved: false},
+
+	// ac4 (farm)
+	{name: "Old MacDonald", description: `Buy a farm<br><i>"Eeieeio"</i>`, achieved: false},
+	{name: "Why are you green?", description: "Buy 5 farms", achieved: false},
+	{name: "A valley with stardew...", description: "Buy 10 farms", achieved: false},
+	{name: "What even grows here?", description: "Buy 25 farms", achieved: false},
+	{name: "Serious dedication", description: "Buy 50 farms", achieved: false},
+	{name: "Young MacDonald", description: "Buy 100 farms", achieved: false},
+
+	// ac5 (factory II)
+	{name: "Womber II", description: "Buy a factory II", achieved: false},
+	{name: "It's eggs!", description: `Buy 2 factory II's<br><i>"It's eggs, woo!"</i>`, achieved: false},
+	{name: "Banker", description: "Buy 10 factory II's", achieved: false},
+	{name: "Birther", description: "Buy 25 factory II's", achieved: false},
+	{name: "FORTY ONE?", description: "Buy 41 factory II's", achieved: false},
+	{name: "Too many!!!", description: "Buy 100 factory II's", achieved: false},
+
+	// ac6 (summoner)
+	{name: "Terraprisma", description: "Buy a summoner", achieved: false},
+	{name: "Tyler, the Creator", description: "Buy 5 summoners", achieved: false},
+	{name: "Commander", description: "Buy 10 summoners", achieved: false},
+	{name: "Stardust", description: "Buy 25 summoners", achieved: false},
+	{name: "Fourth class", description: "Buy 50 summoners", achieved: false},
+	{name: "...I made the summoner", description: `Buy 100 summoners<br><i>"My name is Edwin..."</i>`, achieved: false},
+
+	// ac7 (duplicator)
+	{name: "Command-C", description: "Buy a duplicator", achieved: false},
+	{name: "Command-V", description: "Buy 2 duplicators", achieved: false},
+	{name: "Clone tycoon", description: "Buy 10 duplicators", achieved: false},
+	{name: "Lagspike", description: "Buy 25 duplicators", achieved: false},
+	{name: "Beware of Hausemaster", description: "Buy 50 duplicators", achieved: false},
+	{name: "Ohhh MY PC!!!!!", description: "Buy 100 duplicators", achieved: false},
+
+	// ac8 (wizard)
+	{name: "Alecazam", description: "Buy a wizard", achieved: false},
+	{name: "Abracadabra", description: `Buy 5 wizards<br><i>"...amor-oo-na-na abracadabra, morta-oo-ga-ga, abracadabra, abra-oo-na-na..."</i>`, achieved: false},
+	{name: "Shadow Wizard Money Gang", description: "Buy 10 wizards", achieved: false},
+	{name: "The Wizard & I", description: "Buy 25 wizards", achieved: false},
+	{name: "Bippity boppity boo!", description: "Buy 50 wizards", achieved: false},
+	{name: "We love casting spells.", description: "Buy 100 wizards", achieved: false},
+
+	// ac9 (blessing)
+	{name: "Der Voghormya", description: "Buy a blessing", achieved: false},
+	{name: "Soorp Adzvadz", description: "Buy 5 blessings", achieved: false},
+	{name: "Hayr Mer", description: "Buy 10 blessings", achieved: false},
+	{name: "Jashagetzook", description: "Buy 25 blessings", achieved: false},
+	{name: "Marmin Deroonagan", description: "Buy 50 blessings", achieved: false},
+	{name: "Amen", description: "Buy 100 blessings", achieved: false},
+
+	// ac10 (stealing machine)
+	{name: "Jailbreak", description: "Buy a stealing machine", achieved: false},
+	{name: "Gru", description: `Buy 5 stealing machines<br><i>"Don't tell Fester..."</i>`, achieved: false},
+	{name: "bad guy", description: "Buy 10 stealing machines", achieved: false},
+	{name: "Gangsta's Paradise", description: "Buy 25 stealing machines", achieved: false},
+	{name: "youtu.be/WZa_aZP91wQ?si=HDE1SNvG_aWp-AEe&t=892", description: `Buy 50 stealing machines<br><i>"Hey, Michael, what are your favorite qualities about your wife?"</i>`, achieved: false, clickable: true},
+	{name: "We thugs", description: "Buy 100 stealing machines", achieved: false},
+
+	// ac11 (moon shipment)
+	{name: "A dream that's coming true", description: "Buy a moon shipment", achieved: false},
+	{name: "I GOT ACCEPTED FOR A NASA INTERNSHIP", description: `Buy 5 moon shipments<br><i>"Language."</i>`, achieved: false},
+	{name: "Smells like cheese", description: "Buy 10 moon shipments", achieved: false},
+	{name: "The name alone is gold", description: "Buy 25 moon shipments", achieved: false},
+	{name: "Blood moon plus black substance (for flannel)", description: "Buy 50 moon shipments", achieved: false},
+	{name: `When the moon says "I love you"`, description: "Buy 100 moon shipments", achieved: false},
+
+	// ac12 (treasure room)
+	{name: "Captain Toad", description: "Buy a treasure room", achieved: false},
+	{name: "The third one", description: `Buy 3 treasure rooms<br><i>"First is the worst, second is the best, third... you just be third"</i>`, achieved: false},
+	{name: "Mr. McDuck", description: `Buy 10 treasure rooms<br><i>"ALL I DO IS WIN WIN WIN NO MATTA WHAT oh wait that's Glomgold"</i>`, achieved: false},
+	{name: "Midas from Fortnite", description: "Buy 25 treasure rooms", achieved: false},
+	{name: "Ralecs to raleches", description: "Buy 50 treasure rooms", achieved: false},
+	{name: "The next Mr. Krabs", description: "Buy 100 treasure rooms", achieved: false},
+
+	// ac13 (fiverr offer)
+	{name: "What does this even do?", description: "Buy a Fiverr offer", achieved: false},
+	{name: "Why is this so expensive, anyway?", description: "Buy 5 Fiverr offers", achieved: false},
+	{name: "I have too many questions", description: "Buy 10 Fiverr offers", achieved: false},
+	{name: "How would this even work?", description: "Buy 25 Fiverr offers", achieved: false},
+	{name: "Who's even buying these?", description: "Buy 50 Fiverr offers", achieved: false},
+	{name: "Couch potato", description: "Buy 100 Fiverr offers", achieved: false},
+
+	// ac14 (flannelverse)
+	{name: "Don't tell Feige", description: "Buy a Flannelverse", achieved: false},
+	{name: "Don't tell Sony, either", description: "Buy 5 Flannelverses", achieved: false},
+	{name: "Alec with a Queue", description: "Buy 10 Flannelverses", achieved: false},
+	{name: "HE'S ENTERING SECTOR FOUR!", description: "Buy 25 Flannelverses", achieved: false},
+	{name: "My doctor is really strange", description: `Buy 50 Flannelverses<br><i>"...say that again?"</i>`, achieved: false},
+	{name: "Till you're 90", description: "Buy 90 Flannelverses", achieved: false},
+
+	// ac15 (trillion lumberjacks)
+	{name: "Timber!", description: "Buy a trillion lumberjacks", achieved: false},
+	{name: "Explain the gap in your resume", description: `Buy 5 trillion lumberjacks<br><i>"Yeah, that was when I didn't have a job."</i>`, achieved: false},
+	{name: "Kid named Jack", description: "Buy 10 trillion lumberjacks", achieved: false},
+	{name: "#TeamNoTrees", description: "Buy 25 trillion lumberjacks", achieved: false},
+	{name: "Chopped chin", description: "Buy 50 trillion lumberjacks", achieved: false},
+	{name: "Mr. Worldwide", description: "Buy 100 trillion lumberjacks", achieved: false},
+
+	// ac16 (time flannels)
+	{name: "90's kid", description: "Buy a flannel from another time", achieved: false},
+	{name: "Smells like teen flannels", description: "Buy 5 flannels from another time", achieved: false},
+	{name: "Flannellong", description: "Buy 10 flannels from another time", achieved: false},
+	{name: "SKIBIDI TOILET", description: `Buy 25 flannels from another time<br><i>"Oops, wrong decade..."</i>`, achieved: false},
+	{name: "Alec in Chains", description: "Buy 50 flannels from another time", achieved: false},
+	{name: "The next Kurt Cobain", description: "Buy 100 flannels from another time", achieved: false},
+
+	// ac17 (???)
+
+
+	// ac18 (???)
+
+
+	// ac19 (???)
+
+
+	// ac20 (???)
+];
